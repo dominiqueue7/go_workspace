2788278d21b02eb7c22364987640720760a6a98d
dominiqueue7 <dominiqueue7@gmail.com>
2025-01-14 15:26:48 +0900

    unit test

---
+++ chapter2/add.go
@@ -0,0 +1,6 @@
+package chapter2
+
+// 테스트 대상 함수
+func Add(a, b int) int {
+    return a + b
+}
---
+++ chapter2/add_test.go
@@ -0,0 +1,12 @@
+package chapter2
+
+import "testing"
+
+// Add 함수를 테스트하는 함수
+func TestAdd(t *testing.T) {
+    result := Add(1, 2)
+    expected := 3
+    if result != expected {
+        t.Errorf("Add(1, 2) = %d; want %d", result, expected)
+    }
+}
